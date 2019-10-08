892dd43b85528b0d194a714309cee172bf2d97e9
PStar User Account <pstar@192.168.122.1>
2019-10-08 10:34:19 +0000

    mcoxyhyst_reverse that handles missing data

--- mexec_processing_scripts/depths/mcoxyhyst_reverse.m
+++ mexec_processing_scripts/depths/mcoxyhyst_reverse.m
@@ -10,12 +10,26 @@ function [oxygen_rev]=mcoxyhyst_reverse(oxygen_sbe,time,press,H1,H2,H3)
 % H2      5000      
 % H3      1450      [1200  to  2000]
 
+% DAS chnaged to deal with absent data but note this could give slightly different results from original data as 
+% the sbe software may deal with absent data differently
+
 oxygen_rev=oxygen_sbe;
+
+kfirst = min(find(isfinite(oxygen_sbe)));
+klastgood = kfirst; % keep track of most recent good cycle
 
 for k=2:length(time)
     D=1+H1*(exp(press(k)/H2)-1);
     C=exp(-1*(time(k)-time(k-1))/H3);
 
     oxygen_rev(k)=D*(oxygen_sbe(k)-C*oxygen_sbe(k-1))+C*oxygen_rev(k-1);
-    
+    if isnan(oxygen_sbe(k)+press(k))
+        oxygen_rev(k) = nan; %already the case because of initialisation of oxygen_out
+    else
+        if press(k) < 0; press(k) = 0; end
+        D=1+H1*(exp(press(k)/H2)-1);
+        C=exp(-1*(time(k)-time(klastgood))/H3);
+		oxygen_rev(k)=D*(oxygen_sbe(k)-C*oxygen_sbe(klastgood))+C*oxygen_rev(klastgood);
+        klastgood = k;
+    end    
 end;
