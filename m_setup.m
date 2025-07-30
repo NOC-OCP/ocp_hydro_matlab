ca023cade4545634dc109635bfba74b15b139faf
Yvonne Firing <yvonne.firing@noc.ac.uk>
2025-07-30 15:48:47 +0000

    jc281 rvdas test

--- m_setup.m
+++ m_setup.m
@@ -51,15 +51,15 @@ clear MEXEC_G
 global MEXEC_G
 
 %defaults: what are we processing and where? 
-MEXEC_G.MSCRIPT_CRUISE_STRING='jc282';
+MEXEC_G.MSCRIPT_CRUISE_STRING='jc281';
 MEXEC_G.ix_ladcp = 0; %set to 0 to not add ldeo_ix paths (for instance if processing mooring data)
 MEXEC_G.SITE_suf = 'atsea'; % common suffixes 'atsea', 'athome', '', etc.
 MEXEC_G.perms = [664; 775]; % permissions for files and directories
-MEXEC_G.mexec_data_root = '/data/pstar/projects/obg/jc282/mcruise/data/'; 
+MEXEC_G.mexec_data_root = '/data/pstar/projects/obg/jc281/mcruise/data/'; 
 MEXEC_G.other_programs_root = {'/data/pstar/programs/others/matlab';'/data/pstar/repos/athurnherr'}; 
 MEXEC_G.mexec_shell_scripts = '/data/pstar/repos/NOC-OCP/mexec_exec/';
 MEXEC_G.quiet = 2; %if 0, both file_tools/mexec programs and mexec_processing_scripts will be verbose; if 1, only the latter; if 2, neither
-MEXEC_G.raw_underway = 0; %if 0, skip the rvdas setup (use saved mrtables)
+MEXEC_G.raw_underway = 1; %if 0, skip the rvdas setup (use saved mrtables)
 MEXEC_G.Muse_version_lockfile = 'yes'; % takes value 'yes' or 'no'
 force_vers = 0; %set to 1 to use hard-coded version numbers for e.g. LADCP software, gsw, gamma_n (otherwise finds highest version number available)
 
@@ -181,7 +181,7 @@ switch MEXEC_G.MSCRIPT_CRUISE_STRING(1:2)
     case 'kn'
         MEXEC_G.Mship = 'knorr';
         MEXEC_G.PLATFORM_IDENTIFIER = 'RV Knorr';
-    case 'en'
+    case 'en'            
         MEXEC_G.Mship = 'endeavor';
         MEXEC_G.PLATFORM_IDENTIFIER = 'RV Endeavor';
     otherwise
@@ -225,7 +225,7 @@ try
 catch
     if MEXEC_G.raw_underway
         try
-            fprintf(1,'regenerating mstar-table lookup by running mrdefine(''redo'')')
+            fprintf(1,'regenerating mstar-table lookup by running mrdefine(''redo'')\n')
             switch MEXEC_G.Mshipdatasystem
                 case 'rvdas'
                     mrtv = mrdefine('redo');
