bb3d8ce189956509ba82e41aa4ea847dfe7ae6ba
Yvonne Firing <yvonne.firing@noc.ac.uk>
2026-02-08 17:22:37 +0000

    from main

--- file_tools/mexec/mrvdas/mrgetrvdascontents.m
+++ file_tools/mexec/mrvdas/mrgetrvdascontents.m
@@ -9,7 +9,7 @@ function rvdas_tables = mrgetrvdascontents(varargin)
 % Evolution on that cruise by bak, ylf, pa
 % *************************************************************************
 %
-% Get a list of the entire contents of the rvdas database
+% Access the RVDAS database to get a list of the tables and their variables
 %
 %  Create a table with columns for tablenames and tablevars
 %
@@ -28,7 +28,7 @@ function rvdas_tables = mrgetrvdascontents(varargin)
 %
 % Output:
 %
-% The table returned has two columns, tablenames and tabelvars. Each row in
+% The table returned has two columns, tablenames and tablevars. Each row in
 % tabelvars is a cell array list of the variables in that row's tablename.  
 %
 % So the table names are
@@ -38,26 +38,22 @@ function rvdas_tables = mrgetrvdascontents(varargin)
 %  rvdas_tables.tablevars{strcmp('cnav_gngga',rvdas_tables.tablenames)}
 
 quiet = 1; if nargin>0; quiet = varargin{1}; end
+
+%list of tables
 rvdas_tables = mrgettables(quiet);
+
 ntables = length(rvdas_tables.tablenames);
 rvdas_tables.tablevars = cell(ntables,1);
-
-iis = [];
+keep = true(ntables,1);
 for kl = 1:ntables
-    tabname = rvdas_tables.tablenames{kl};
-    
-    if strcmp(tabname,'logta')
-        % this is not a proper variable
-        continue
-    end
-    
-    tablevars = mrgettablevars(tabname,quiet);
+    %variables for each table
+    tablevars = mrgettablevars(rvdas_tables.tablenames{kl},quiet);
     if isempty(tablevars)
-        warning('table %s does not exist or has no variables; skipping',tabname)
-        iis = [iis kl];
+        warning('table %s does not exist or has no variables; skipping',rvdas_tables.tablenames{kl})
+        keep(kl) = false;
     else
         rvdas_tables.tablevars{kl} = tablevars;
     end
         
 end
-rvdas_tables(iis,:) = [];
+rvdas_tables = rvdas_tables(keep,:);
