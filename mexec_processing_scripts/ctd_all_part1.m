3c93d814bdc488c0b2485dcf6d3a8958997575bd
Yvonne Firing <yvonne.firing@noc.ac.uk>
2026-06-23 12:53:09 +0000

    reorg adjustment 1/?

--- mexec_processing_scripts/ctd_all_part1.m
+++ mexec_processing_scripts/ctd_all_part1.m
@@ -1,18 +1,18 @@
-mctd_01(stn); %read in sbe .cnv data to mstar
+msbe_01_load(stn); %read in sbe .cnv data to mstar
 opt1 = 'ctd_proc'; opt2 = 'ctd_raw_extra'; get_cropt
 if exist('ctd_raw_extra','var')
     eval(ctd_raw_extra)
 end
-mfir_01(stn) %sbe .bl file to mstar
+mfir_01_load(stn) %sbe .bl file to mstar
 
 %apply corrections (e.g. oxygen hysteresis) and calibrations, as specified in opt_cruise
-mctd_02(stn); 
+msbe_02_edcal(stn); 
 
-mctd_03(stn); %average to 1 hz, compute salinity
+msbe_03_1hz(stn); %average to 1 hz, compute salinity
 
 global MEXEC_G
 if isfield(MEXEC_G,'ix_ladcp') && MEXEC_G.ix_ladcp
     mout_1hzasc(stn) %output 1 hz data in ascii format (required for LDEO IX LADCP processing)
 end
 
-mdcs_01(stn); % now does mdcs_01 and mdcs_02 in one step
+mdcs_01_auto(stn); % now does mdcs_01 and mdcs_02 in one step
