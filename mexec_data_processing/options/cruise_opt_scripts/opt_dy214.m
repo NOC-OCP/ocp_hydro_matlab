682926da6df26f69b142af5ae4e2e787dd0ae753
Kristin Burmeister <kristin.burmeister@sams.ac.uk>
2026-09-02 06:01:53 +0000

    Resolve merge conflict after stash pop

--- mexec_data_processing/options/cruise_opt_scripts/opt_dy214.m
+++ mexec_data_processing/options/cruise_opt_scripts/opt_dy214.m
@@ -198,7 +198,9 @@ switch opt1
                 yl.press = [-1 3200];
                 yl.press = [-1 ceil(d.press(ddcs.dc24_bot)/100)*100+10];
                 yl.fluor = [0 8]; yl.par = [0 40];
-                if stn==1
+                if ismember(stn,[1,2])
+                    yl.cond = [40 50];
+                    yl.press = [-2 150];
                     yl.temp = [15 25];
                 end
                 if ismember(stn,[1,2])
